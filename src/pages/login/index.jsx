33ccd407f64223cb66b3f93482ad341ead76d113
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-08-10 22:26:28 -0500

    Add basic login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,41 @@
+import { useState } from 'react'
+import './login.css'
 
 function Login() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    // TODO: Lógica de Autenticación
+    console.log('Usuario: ', userName)
+    console.log('Password: ', password)
+  }
+
   return (
-    <>
-      <div>
-        <p>Login</p>
-      </div>
-    </>
+    <div className='container'>
+      <form onSubmit={handleLogin}>
+        <div className='input-group'>
+          <label>Usuario</label>
+          <input
+            type='text'
+            value={userName}
+            onChange={(event) => setUserName(event.target.value)}
+          />
+        </div>
+        <div className='input-group'>
+          <label>Contraseña</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type='submit'>
+          ACCEDER
+        </button>
+      </form>
+    </div>
   )
 }
 
