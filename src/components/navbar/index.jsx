e125b79e63a43cd0c1dbb3b0b0decb29b3369b77
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-08-12 22:27:17 -0500

    Add link to routes

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,36 +1,71 @@
+import { NavLink } from 'react-router-dom'
 import './navbar.css'
 
 function Navbar() {
   return (
     <nav className='navbar'>
       <div className='logo'>Soft-ID</div>
-      <ul className='menu'>
-        <li className='menu-item'>
-          Configuración
-          <ul className='dropdown'>
-            <li className='dropdown-item'> -Roles </li>
-            <li className='dropdown-item'> -Funciones </li>
-            <li className='dropdown-item'> -Aplicaciones </li>
-            <li className='dropdown-item'> -Gerencias </li>
-            <li className='dropdown-item'> -Direcciones </li>
-            <li className='dropdown-item'> -Usuarios </li>
-          </ul>
-        </li>
-        <li className='menu-item'>
-          RRHH
-          <ul className='dropdown'>
-            <li className='dropdown-item'> -Empleados </li>
-          </ul>
-        </li>
-        <li className='menu-item'>
-          Reportes
-          <ul className='dropdown'>
-            <li className='dropdown-item'> -Generador de informes </li>
-          </ul>
-        </li>
-      </ul>
+      <div className='container-menu'>
+        <ul className='menu'>
+          <li className='menu-item'>
+            Configuración
+            <ul className='dropdown'>
+              <li className='dropdown-item'>
+                <NavLink to='/roles'>
+                  -Roles
+                </NavLink>
+              </li>
+              <li className='dropdown-item'>
+                <NavLink to='/funciones'>
+                  -Funciones
+                </NavLink>
+              </li>
+              <li className='dropdown-item'>
+                <NavLink to='/apps'>
+                  -Aplicaciones
+                </NavLink>
+              </li>
+              <li className='dropdown-item'>
+                <NavLink to='/gerencia'>
+                  -Gerencias
+                </NavLink>
+              </li>
+              <li className='dropdown-item'>
+                <NavLink to='/direccion'>
+                  -Direcciones
+                </NavLink>
+              </li>
+              <li className='dropdown-item'>
+                <NavLink to='/usuario'>
+                  -Usuarios
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className='menu-item'>
+            RRHH
+            <ul className='dropdown'>
+              <li className='dropdown-item'>
+                <NavLink to='/empleado'>
+                  -Empleados
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className='menu-item'>
+            Reportes
+            <ul className='dropdown'>
+              <li className='dropdown-item'>
+                <NavLink to='/reporte'>
+                  -Generador de informes
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </nav>
-  );
+  )
 }
 
 export default Navbar
