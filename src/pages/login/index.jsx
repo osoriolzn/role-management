92f72c334e293a0f8284cc97a112839845574c22
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-08-14 22:19:14 -0500

    Add logic a component

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,39 +1,72 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { Navigate } from 'react-router-dom'
+import responseMock from '../../mocks/response.api.json'
 import './login.css'
 
+// const URL = 'https//url-del-api.com/user'
+
 function Login() {
-  const [userName, setUserName] = useState('')
-  const [password, setPassword] = useState('')
+  const [users, setUsers] = useState({})
+  const [userIsLogin, setUserIsLogin] = useState(false)
+  const form = useRef(null)
+
+  useEffect(() => {
+    setUsers(responseMock)
+    // fetch(URL)
+    //   .then(response => response.json())
+    //   .then(data => setUsers(data))
+  }, [])
 
   const handleLogin = (event) => {
     event.preventDefault()
-    // TODO: Lógica de Autenticación
-    console.log('Usuario: ', userName)
-    console.log('Password: ', password)
+    const formOfData = new FormData(form.current)
+    const data = {
+      userName: formOfData.get('username'),
+      password: formOfData.get('userpass')
+    }
+    userIsValid(data)
+  }
+
+  const userIsValid = (loginData) => {
+    const user = users.find((u) => u.usuario === loginData.userName && u.password === loginData.password)
+
+    if (user) {
+      const stringifiedLogin = JSON.stringify(true)
+      localStorage.setItem('login', stringifiedLogin)
+      setUserIsLogin(true)
+    } else {
+      console.log('Usuario o contraseña no validos')
+    }
+  }
+
+  if (userIsLogin) {
+    return <Navigate replace to='/' />
   }
 
   return (
     <>
       <img className='background-image' src='src/assets/img/login.webp' alt='Imagen de fondo'/>
       <div className='container'>
-        <form onSubmit={handleLogin}>
+        <form ref={form}>
           <div className='input-group'>
-            <label>Usuario</label>
+            <label htmlFor='username'>Usuario</label>
             <input
+              name='username'
+              id='username'
+              autoComplete='true'
               type='text'
-              value={userName}
-              onChange={(event) => setUserName(event.target.value)}
             />
           </div>
           <div className='input-group'>
-            <label>Contraseña</label>
+            <label htmlFor='userpass'>Contraseña</label>
             <input
+              name='userpass'
+              id='userpass'
+              autoComplete='true'
               type='password'
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
             />
           </div>
-          <button type='submit'>
+          <button onClick={handleLogin}>
             ACCEDER
           </button>
         </form>
