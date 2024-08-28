39161168d20264668482b09102f1833b3915d5a2
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-08-27 21:43:13 -0500

    Add positions view

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,6 +1,7 @@
 import { useRoutes, Navigate, BrowserRouter } from 'react-router-dom'
 import Home from '../home'
 import Login from '../login'
+import Positions from '../positions'
 import RolesApp from '../roles-app'
 import Users from '../users'
 import NotFound from '../not-found'
@@ -12,6 +13,7 @@ const AppRoutes = () => {
     { path: '/', element: userIsLogin ? <Navigate replace to={'/home'} /> : <Navigate replace to={'/acceso'} /> },
     { path: '/home', element: userIsLogin ? <Home /> : <Navigate replace to={'/acceso'} /> },
     { path: '/acceso', element: userIsLogin ? <Navigate replace to={'/home'} /> : <Login /> },
+    { path: '/cargos', element: userIsLogin ? <Positions /> : <Login /> },
     { path: '/roles-app', element: userIsLogin ? <RolesApp /> : <Login /> },
     { path: '/usuarios', element: userIsLogin ? <Users /> : <Login /> },
     { path: '/*', element: <NotFound /> },
---
+++ src/pages/positions/index.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect, useRef } from 'react'
+import Navbar from '../../components/navbar'
+import Layout from '../../components/layout'
+import './positions.css'
+
+const API_URL = 'http://localhost:3000/api/v1/cargos'
+
+function Positions() {
+  const [positions, setPositions] = useState([])
+  const form = useRef(null)
+  
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setPositions(data))
+  }, [])
+
+  return (
+    <>
+      <Navbar />
+      <Layout
+        image={
+          <figure>
+            <img
+              className='cargo-img'
+              src='src/assets/img/roles.webp'
+              alt='logo de la página'
+            />
+          </figure>
+        }
+        form={
+          <form ref={form}>
+            <div className='input-group'>
+              <label htmlFor='cargo'>Nombre del Cargo</label>
+              <input
+                name='cargo'
+                id='cargo'
+                autoComplete='true'
+                type='text'
+              />
+            </div>
+            <div className='input-group'>
+              <label htmlFor='status'>Estado</label>
+              <input
+                name='status'
+                id='status'
+                autoComplete='true'
+                type='text'
+              />
+            </div>
+            <div className='input-group'>
+              <label htmlFor='manager'>Gerencia</label>
+              <input
+                name='manager'
+                id='manager'
+                autoComplete='true'
+                type='text'
+              />
+            </div>
+            <div className='input-group'>
+              <label htmlFor='department'>Dirección</label>
+              <input
+                name='department'
+                id='department'
+                autoComplete='true'
+                type='text'
+              />
+            </div>
+            <button className='access'>GUARDAR</button>
+          </form>
+        }
+        data={
+          <table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nombre Cargo</th>
+                <th>F. Creación</th>
+                <th>Estado</th>
+                <th>Gerencia</th>
+                <th>Dirección</th>
+              </tr>
+            </thead>
+            <tbody>{positions.map(position => (
+              <tr key={position.id_cargo}>
+                <td>{position.id_cargo}</td>
+                <td>{position.nombre}</td>
+                <td>{position.fecha_creacion}</td>
+                <td>{position.estado}</td>
+                <td>{position.id_gerencia}</td>
+                <td>{position.id_direccion}</td>
+              </tr>
+            ))}</tbody>
+          </table>
+        }
+      />
+    </>
+  )
+}
+
+export default Positions
