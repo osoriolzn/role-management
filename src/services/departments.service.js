85c03faca1003af59d1b7b27e30474b421e227c8
Yohan D Osorio l <yooslzn1487@gmail.com>
2024-08-30 14:28:31 -0500

    Refactor in service file

--- src/services/departments.service.js
+++ src/services/departments.service.js
@@ -3,17 +3,17 @@ import axios from 'axios'
 const API_URL = 'http://localhost:3000/api/v1/direcciones'
 
 class EndpointsDepartments {
-    constructor() {}
-  
-    async getDepartments() {
-      const response = await axios.get(API_URL)
-      return response
-    }
-  
-    async createDepartment(department) {
-      const response = await axios.post(API_URL, department)
-      return response
-    }
+  constructor() {}
+
+  async getDepartments() {
+    const response = await axios.get(API_URL)
+    return response
   }
-  
-  export default EndpointsDepartments
+
+  async createDepartment(department) {
+    const response = await axios.post(API_URL, department)
+    return response
+  }
+}
+
+export default EndpointsDepartments
--- src/services/managers.service.js
+++ src/services/managers.service.js
@@ -3,17 +3,17 @@ import axios from 'axios'
 const API_URL = 'http://localhost:3000/api/v1/gerencias'
 
 class EndpointsManagers {
-    constructor() {}
-  
-    async getManagers() {
-      const response = await axios.get(API_URL)
-      return response
-    }
-  
-    async createManager(manager) {
-      const response = await axios.post(API_URL, manager)
-      return response
-    }
+  constructor() {}
+
+  async getManagers() {
+    const response = await axios.get(API_URL)
+    return response
   }
-  
-  export default EndpointsManagers
+
+  async createManager(manager) {
+    const response = await axios.post(API_URL, manager)
+    return response
+  }
+}
+
+export default EndpointsManagers
